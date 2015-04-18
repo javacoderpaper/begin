1cf29d9f58780b21cc4fe1fac56906264c4e1fee
coderpaper <javacoderpaper@gmail.com>
2015-04-18 22:44:13 +0600

    Hello World

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by CoderPaper on 18.04.15.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.println(String.format("%s", "Everything will be fine"));
+    }
+}
