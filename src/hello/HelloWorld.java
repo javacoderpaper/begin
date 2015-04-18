39d8b5446d58a5f316018da4818ccea3f34fb107
coderpaper <javacoderpaper@gmail.com>
2015-04-18 22:58:16 +0600

    1

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ package hello;
  */
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        System.out.println("Hello World !!");
         System.out.println(String.format("%s", "Everything will be fine"));
     }
 }
